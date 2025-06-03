452340488db3bd45d2d83e7b73dd0f779e058657
Nurvyy217 <mh217512@gmail.com>
2025-06-03 07:50:09 +0700

    delete admin feature

--- src/include/login.h
+++ src/include/login.h
@@ -1,33 +1,52 @@
 #ifndef LOGIN_H
 #define LOGIN_H
+
 #include <stdio.h>
 #include "transitKota.h"
 
+// Path file user
 #define USER_FILE "data/user.txt"
 
+// Struktur data user
 typedef struct {
     char username[50];
     int pin;
     int saldo;
 } User;
 
+// Inisialisasi file user (buat jika belum ada)
 void initUserFile();
-void topUp(User *user);
-void menuAdmin();
+
+// Registrasi user baru
+void registrasi();
+
+// Login user
+void loginUser(TreeManager *tm);
+
+// Simpan user baru ke file
+void simpanUser(User *u);
+
+// Cek apakah username sudah ada
+int cekUsernameSudahAda(const char *username);
+
+// Ambil data user berdasarkan username
+int getUserByUsername(const char *username, User *u);
+
+// Menu utama user
 void menuUser(User *user, TreeManager *tm);
+
+// Top up saldo user
+void topUp(User *user);
+
+// Fungsi bantu set/get user
 void setUsername(User *n, const char *username);
 void setPin(User *n, int pin);
 void setSaldo(User *n, int saldo);
 int getPin(User *n);
 char *getUsername(User *n);
 int getSaldo(User *n);
-void simpanUser(User *u);
-void registrasi();
-void loginAdmin();
-int cekUsernameSudahAda(const char *username);
-void loginUser(TreeManager *tm);
-int getUserByUsername(const char *username, User *u);
+
+// Fitur beli produk (sementara)
 void beliProduk(TreeManager *tm);
-void inputPin(int *pin);
 
 #endif
--- src/main/main.c
+++ src/main/main.c
@@ -6,31 +6,22 @@
 
 int main() {
     TreeManager tm;
-    
-    // Initialize files
     initUserFile();
     InitTree(&tm);
     
-    char choice1, choice2;
+    char choice;
+    system("cls");
     printf("\n\n\n\n\t\t\t\t\t\tWelcome to online shop\n");
     Sleep(3000);
     system("cls");
-    printTopCenter("LOGIN / REGISTRASI\n");
-    printf("\t\t\t\t\t\t\t<=====================================>\n");
-    printf("\n\n\n\nPilihan: ");scanf(" %c", &choice1);
+    printf("\t\t\t\t\t\t\t1.LOGIN / 2.REGISTRASI\n");
+    printf("\t\t\t\t\t\t<==================================>\n");
+    printf("\n\n\n\nPilihan: ");scanf(" %c", &choice);
     system("cls");
-    if (choice1 == '1') {
-        printf("\t\t\t\t\t\t   1.ADMIN / 2. USER\n");
-        printf("\t\t\t\t\t<=====================================>\n");
-        printf("\n\n\n\nPilihan: ");scanf(" %c",&choice2);
+    if (choice == '1') {
         system("cls");
-        if(choice2 == '1'){
-            loginAdmin();
-        }
-        else if(choice2 == '2'){
-            loginUser(&tm);
-        }
-    } else if (choice1 == '2') {
+        loginUser(&tm);
+    } else if (choice == '2') {
         system("cls");
         registrasi();
     } else {
