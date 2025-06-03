34f41c2e598b25bc6d6b677cf76f625a08a25597
nabilmudz <nabilmudzaki306@gmail.com>
2025-06-03 13:59:40 +0700

    add: admin entrypoint

---
+++ src/main/admin.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <windows.h>
+#include "../include/styleText.h"
+#include "../include/login.h"
+#include "../include/transitKota.h"
+#include "../include/environment.h"
+
+int main() {
+    print_center("SELAMAT DATANG ADMIN", WIDTH, HEIGHT);
+
+    return 0;
+}
--- src/main/user.c
+++ src/main/user.c
@@ -4,10 +4,10 @@
 #include "printTemplate.h"
 #include "login.h"
 #include "katalog.h"
-#include "environment.h"
+#include "../include/environment.h"
 
 int main() {
-    clear_screen();
-    print_center("MODUL USER", WIDTH, HEIGHT);
+    print_center("SELAMAT DATANG USER", WIDTH, HEIGHT);
+    
     return 0;
 }
